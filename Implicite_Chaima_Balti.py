d52d1818b9a1b22a5e9ee3324ca969865a92c8db
ChaimaBalti <baltichaima09@gmail.com>
2024-02-13 09:42:53 +0100

    add implicite and update explicite

--- Implicite_Chaima_Balti.py
+++ Implicite_Chaima_Balti.py
@@ -1,23 +1,25 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on FEV 06 2024
+Created on FEV 13 2024
 
 @author: Chaima Balti 2AMIndS
 """
 #******************************************************************************
 # Résolution numérique de l'équation de transport
 # u,t + c u,x = 0
-# avec des schémas explicites
+# avec des schémas implécite
 #******************************************************************************
-
 
 # Load packages----------------------------------------------------------------
 
 import numpy as np
 import matplotlib.pyplot as plt 
+import scipy as sp
+from scipy.sparse import csc_matrix
 
 # Paramètres du probleme-------------------------------------------------------
+
 
 # Parametres de discretisation
 h  = 0.01
@@ -31,24 +33,22 @@ cond_ini =0
 # Vitesse
 #   0 => constante  (cas i)
 #   1 => variable   (cas ii)
-vitesse = 0
+vitesse = 1
 
-# Type de schéms
-# 1 => schéma d écentré
-# 2 => schéma Lax-Wendroff,
-typsch=1
 
 # Creation des maillages-------------------------------------------------------
-# Espace
-x = np.arange(0,1+2*h,h) ;   # Le point d'indice Nx-1 correspond a x=1, identique a x=0 d'apres les CL
-Nx = np.size(x);             # Le point d'indice Nx correspond a x=1+h, identique a x=h d'apres les CL
 
-# Temps
+x = np.arange(0,1,h);   # Le point d'indice Nx-1 correspond a x=1-h, identique a x=h d'apres les CL
+Nx =len(x);             # Le point d'indice Nx correspond a x=1, identique a x=0 d'apres les CL
+
+
 t = np.arange(0,1+dt,dt);
-Nt = np.size(t);
+Nt = len(t);
 
 # Initialisation---------------------------------------------------------------
+
 U = np.zeros([Nt, Nx]);
+
 
 def gaussienne(x):
     '''% (avec 0 <= x <= 1)
@@ -70,101 +70,79 @@ def chapeau(x):
     y=x*mask1+(0.5-x)*mask2  
     return y
         
+
 if  cond_ini==0:
-        U[0,:]=gaussienne(x);
-else:
-        U[0,:]=chapeau(x);
+    U[0,:]=gaussienne(x);   
+else: 
+    U[0,:]=chapeau(x);
+
 
 # Boucle en temps--------------------------------------------------------------
 
-for n in np.arange(0,Nt-1):
+print(U)
+for n in np.arange(0,Nx):
     if vitesse == 0:
         c=0.8                # cas (i)
     else:
-        c=np.sin(10*t[n])    # cas (ii)
+        c=np.sin(10*t[n+1])    # cas (ii)
+    alpha = c*(dt/h)
     
+    A = csc_matrix(np.diag((1+alpha)*np.ones(Nx-1),k=1)+np.diag((1-alpha)*np.ones(Nx)))
+    A[Nx-1,0]=1+alpha
+    B = csc_matrix(np.diag((1-alpha)*np.ones(Nx-1),k=1)+np.diag((1+alpha)*np.ones(Nx)))
+    B[Nx-1,0]=1-alpha
     
-    alpha = c*dt/h;
-    if typsch==1:
-        if (c<0):
-            for j in range(Nx-2):
-            # Le schéma décentré si c<0:
-                U[n+1,j] = (1+alpha)*U[n,j]-alpha*U[n,j+1]
-           
-            # Traitement des conditions aux limites periodiques:  
-                U[n+1,Nx-2]=U[n+1,0]
-                U[n+1,Nx-1]=U[n+1,1]
-        else:
-            for j in range(1,Nx-1):
-            # Le schéma décentré si c>=0:
-                U[n+1,j] = (1-alpha)*U[n,j]+alpha*U[n,j-1]
-    
-            # Traitement des conditions aux limites periodiques:
-                U[n+1,0]=U[n+1,Nx-2]
-                U[n+1,Nx-1]=U[n+1,1]
-        
-    elif typsch==2:
-        # Le schéma de Lax-Wendro:
-        for j in range(1,Nx-1):
-            U[n+1,j] =(1-alpha**2)*U[n,j]+0.5*alpha*(alpha-1)*U[n,j+1]+0.5*alpha*(alpha+1)*U[n,j-1]
-            
-            # Traitement des conditions aux limites periodiques:
-            U[n+1,0]=U[n+1,Nx-2]
-            U[n+1,Nx-1]=U[n+1,1]
-        
-        
-        
+    U[n+1,:]=sp.sparse.linalg.spsolve(A,B.dot(U[n,:]))
+
+
 #------------------------------------------------------------------------------
 # Le code qui suit ne doit pas etre modifier dans le cadre du TP   
 
 # Solution exacte
 # Uex(t,X(t)) = U0(X(0)) = U0(x-d)
 # avec
-# d = X(t) - X(0) = \int_0^t c(t) dt    
-    
+# d = X(t) - X(0) = \int_0^t c(t) dt      
 
-# Calcule de la solution exacte Uex et de l'erreur avec differentes normes (norme ponctuelle, norme 2, norme infini):
-Uex = np.zeros([Nt, Nx])
-err = np.zeros([Nt, Nx]) # l'erreur ponctuelle entre la solution exacte et approcher en tous points de calcules
-err2 = np.zeros([Nt, 1]) # l'erreur en norme 2 entre la solution exacte et approcher à chaque instant
+
+
+Uex = np.zeros([Nt, Nx]);
+err = np.zeros([Nt, Nx]);
+err2 = np.zeros([Nt, 1]);
 for n in np.arange(0,Nt):
     for j in np.arange(0,Nx):
         if vitesse == 0:
             d=c*t[n]
         else:
             d=0.1*(1.-np.cos(10*t[n]))
-   
-        if cond_ini == 0:
-            Uex[n,j]=gaussienne(np.mod(x[j]-d,1))
-        else:
-            Uex[n,j]=chapeau(np.mod(x[j]-d,1))
-        err[n,j] = U[n,j] - Uex[n,j]
         
-    err2[n] = np.linalg.norm(err[n,:], 2)     
+        if cond_ini == 0:
+            Uex[n,j]=gaussienne(np.mod(x[j]-d,1));
+        else:
+            Uex[n,j]=chapeau(np.mod(x[j]-d,1));
+            
+            
+        err[n,j] = U[n,j] - Uex[n,j];
+    err2[n] = np.linalg.norm(err[n,:], 2);
+        
+err_inf_2 = np.max(err2)            
 
-err_inf_2 = np.max(err2)     
 
-
-# Plots:-----------------------------------------------------------------------
 plt.figure(1)
+#matplotlib.rcParams.update({'font.size': 11, 'font.family': 'serif'})
+
 for n in np.arange(0,Nt):
-    if n%2 == 0:
+    if n%3 == 0:
         plt.plot(x, U[n,:],'b*-',x,Uex[n,:],'ro-')
         plt.xlabel('$x$',fontsize=11)
-        if typsch==1:
-            # Le schéma décentré 
-            plt.title('Schema explicite décentré, $t=$%s' %(round(t[n],3)),fontsize=11)
-        elif typsch==2:
-            # Le schéma de Lax-Wendroff:
-            plt.title('Schema explicite de Lax-Wendroff, $t=$%s' %(round(t[n],3)),fontsize=11)
+        plt.title('Schema explicite centre, $t=$%s' %(t[n]),fontsize=11)
         plt.legend(['Solution approcher','Solution exacte'], loc='best',fontsize=11)
-        plt.pause(0.01)
+        plt.pause(0.1)
 plt.show()
 
 XX,tt=np.meshgrid(x,t)
 fig = plt.figure(figsize=(18,6))
 ax = fig.add_subplot(1,1,1, projection='3d')
-contour=ax.plot_surface(XX,tt,U,rstride=4, cstride=4,alpha=1,cmap='viridis')
+contour=ax.plot_surface(XX,tt,U,rstride=4, cstride=4,alpha=1)
 #contour=ax.plot_trisurf(XX,tt,U)
 #contour=ax.contourf(XX,tt,U)
 
@@ -197,4 +175,5 @@ ax1.set_xlabel('x',fontsize=24)
 ax1.set_ylabel('t',fontsize=24)
 plt.title('Courbes caracteristiques',fontsize=24)
 fig1.show()
+
 
